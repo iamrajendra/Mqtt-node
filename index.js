2fbdac064c7c46fb7e694214a22a489a3a0ea241
iamrajendra <rajendrarajaram@hotmail.com>
2020-03-22 20:37:10 +0530

    a estable connection is creeated

--- index.js
+++ index.js
@@ -1,14 +1,53 @@
-var mqtt = require('mqtt')
-var client  = mqtt.connect('mqtt://test.mosquitto.org')
- 
-client.on('connect', function () {
-    console.log('MQTT IS CONNECTED')
-  client.subscribe('presence', function (err) {
-    if (!err) {
-      client.publish('presence', 'Hello mqtt')
-    }else{
-        console.log('Error is %s',err)
-    }
+var mqtt = require('mqtt');
+var Topic = 'rajendra'; //subscribe to all topics
+var Broker_URL = 'mqtt://test.mosquitto.org';
 
-  })
-})
+var options = {
+	//clientId: 'rajendra',
+	//port: 1883,
+	//username: 'mqtt_user',
+	//password: 'mqtt_password',	
+	keepalive : 60
+};
+
+var client  = mqtt.connect(Broker_URL, options);
+client.on('connect', mqtt_connect);
+client.on('reconnect', mqtt_reconnect);
+client.on('error', mqtt_error);
+client.on('message', mqtt_messsageReceived);
+client.on('close', mqtt_close);
+
+function mqtt_connect() {
+    console.log("Connecting MQTT");
+    client.subscribe(Topic, mqtt_subscribe);
+}
+
+function mqtt_subscribe(err, granted) {
+    console.log("Subscribed to " + Topic);
+    if (err) {console.log(err);}
+}
+
+function mqtt_reconnect(err) {
+    console.log("Reconnect MQTT");
+    if (err) {console.log(err);}
+	client  = mqtt.connect(Broker_URL, options);
+}
+
+function mqtt_error(err) {
+    console.log("Error!");
+	if (err) {console.log(err);}
+}
+
+function after_publish() {
+	//do nothing
+}
+
+function mqtt_messsageReceived(topic, message, packet) {
+	console.log('Message received = ' + message);
+	//insert_message(topic, message, packet);
+}
+
+function mqtt_close() {
+	console.log("Close MQTT");
+}
+
